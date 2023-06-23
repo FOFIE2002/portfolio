4281d385d08638fa679aa4682e34e5488eee5bdd
fofiefopaelisabeth <119090806+FOFIE2002@users.noreply.github.com>
2023-06-23 22:43:15 +0100

    new changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio.png'
-import IMG2 from '../../assets/foodlie.png'
+// import Swiper core and required modules
+import { Pagination, Navigation, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Portfolio = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://nice-lime-hatchling-sari.cyclic.app/api/portfolio")
+      .then((response) => {
+        const formattedData = response.data.data.map((item) => ({
+          id: item._id,
+          image: item.image,
+          title: item.title,
+          github: item.github,
+          demo: item.demo,
+        }));
+        setData(formattedData);
+      })
+      .catch((error) => {
+        console.log("error fetching portfolio data:", error);
+      });
+  }, []);
   return (
     <section id= "portfolio">
       <h5>My recent work</h5>
       <h2>Portfolio</h2>
       
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG2} alt='foodlie'/>
-          </div>
-          <h3>Foodlie</h3>
-          <div className='portfolio__item-cta'>
-          <a href='https://github.com/FOFIE2002/Foodlie' className='btn' target='_blank' rel="noreferrer">Github</a>
-         
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='portfolio'/>
-          </div>
-          <h3>My Portfolio</h3>
-          <div className='portfolio__item-cta'>
-          <a href='https://github.com/FOFIE2002/portfolio' className='btn' target='_blank' rel="noreferrer">Github</a>
-          <a href='https://fofie-portfolio.netlify.app' className='btn btn-primary'target='_blank' rel="noreferrer">Live Demo</a>
-          </div>
-        </article>
+      {
+              data.map(({ id, image, title, github, demo }) => (
+          <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div> 
+            <h3>{title}</h3> 
+            <div className=".portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                >
+                  Live Demo
+                </a>
+              </div>
+              </article>
+              ))
+            }            
       </div>
     </section>
   )
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './testimonial.css'
 
-const testimonial = () => {
+// import Swiper core and required modules
+import { Pagination, Navigation, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+export const Testimonial = () => {
+  const[data, setData] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get("https://nice-lime-hatchling-sari.cyclic.app/api/testimonial")
+        .then((response) => {
+          const formattedData = response.data.data.map((item) => ({
+            id: item._id,
+            avatar: item.avatar,
+            name: item.name,
+            review: item.review,
+          }));
+          setData(formattedData);
+        })
+        .catch((error) => {
+          console.log("error fetching portfolio data:", error);
+        });
+    }, []);
+
   return (
-    <section id= "testimonial">
-      <h5>Review from clients</h5>
-      <h2>Testimonial</h2>
+    
+    <section id="testimonial">
+      <h5>Review from client</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      modules={[Pagination, Navigation, Scrollbar, A11y]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+      {
+        data.map(({ id, avatar, name, review }) => (
+        
+        <SwiperSlide key={id} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar} alt={name} />
+          </div>
+          <h5>{name}</h5>
+          <small className="client__review">{review}</small>
+        </SwiperSlide>
+      ))}
+      </Swiper>
     </section>
-  )
-}
-
-
-export default testimonial
+  );
+};
+export default Testimonial;
